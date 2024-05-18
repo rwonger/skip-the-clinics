a076ffeefd06a2043961bb7f782a060dfe80e67d
Lynn Nguyen <ngocngan.9a2.2015@gmail.com>
2024-05-18 16:54:42 -0700

    Downgrade to old Angular

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,7 +4,9 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'skip-the-clinics-client' title`, () => {
+  it(`should have as title 'remult-angular-todo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('skip-the-clinics-client');
+    expect(app.title).toEqual('remult-angular-todo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, skip-the-clinics-client');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('remult-angular-todo app is running!');
   });
 });
