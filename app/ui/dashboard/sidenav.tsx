1df291b39f238768a06257ff3e581067a3cfb1bc
Lynn Nguyen <ngocngan.9a2.2015@gmail.com>
2024-05-19 03:32:31 -0700

    Fix color

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -7,7 +7,7 @@ export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
-        className="mb-2 flex h-20 items-end justify-start rounded-md bg-orange-600 p-4 md:h-40"
+        className="mb-2 flex h-20 items-end justify-start rounded-md bg-clinicCustomColor-400 p-4 md:h-40"
         href="/"
       >
         <div className="w-32 text-white md:w-40">
